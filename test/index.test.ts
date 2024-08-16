f36b6c2bb31e9d74be3c72675e4afe56bfe56051
Tanke6003 <Ruben.Farias.1999@Outlook.es>
2024-08-16 12:39:09 -0600

    [ADD] some tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,16 @@
 import { CronTaskScheduler, Task } from '../src/index'
+
 describe('CronTaskScheduler', () => {
     let scheduler: CronTaskScheduler;
-  
+    const onTickMock = jest.fn(); 
+    let task:Task = {
+      name: 'Test Task',
+      interval: 1000,
+      onTick: onTickMock,
+    }
     beforeEach(() => {
       scheduler = new CronTaskScheduler();
+      task = scheduler.addTask(task);
       jest.useFakeTimers(); 
     });
   
@@ -12,28 +19,38 @@ describe('CronTaskScheduler', () => {
     });
   
     test('should add a task', () => {
-      const onTickMock = jest.fn(); 
-      const task = scheduler.addTask({
-        name: 'Test Task',
+      let task2:Task  = {
+        name: 'Test Task 2',
         interval: 1000,
         onTick: onTickMock,
-      });
-      
-      const taskId = task.id ?? '';
-      expect(scheduler.getTaskById(taskId)).toBeDefined();
+      }
+      task2 = scheduler.addTask(task2);
+      expect(scheduler.getTaskById(task.id!)).toBeDefined();
     });
     //getTasks
     test('should have one task',()=>{
-        const onTickMock = jest.fn();
-        scheduler.addTask({
-            name: 'Test Task',
-            interval: 1000,
-            onTick: onTickMock,
-          });
         let tasks:Task[] = scheduler.getTasks()
         expect(tasks.length).toBe(1)
           
     })
+    //getTaskById
+    test('should get a task by id',()=>{
+      task = scheduler.addTask(task);
+      let taskget = scheduler.getTaskById(task.id!)
+      expect(taskget).toBe(task);
+    })
+    test('should remove a task by id',()=>{
+      scheduler.removeTaskById(task.id!)
+      let tasks = scheduler.getTasks();
+      let findtask = tasks.find((x)=>x.id === task.id)
+      expect(findtask).toBe(undefined);
+    })
+    test('should return an error when trying to remove a task by id', () => {
+      expect(() => {
+          scheduler.removeTaskById('failid');
+      }).toThrow('Task not found');
+  });
+  
     
 
   });
